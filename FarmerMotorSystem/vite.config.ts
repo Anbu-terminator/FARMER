7e393334b6750187058405f607b5fbfc74976406
Anbu Sivam.B <anbulegend101@gmail.com>
2025-05-20 12:08:10 +0530

    vite.config.ts

--- FarmerMotorSystem/vite.config.ts
+++ FarmerMotorSystem/vite.config.ts
@@ -3,15 +3,16 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  root: 'client', // <--- Pointing to the client folder
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@assets': path.resolve(__dirname, './public/images'),
+      '@': path.resolve(__dirname, './client/src'),
+      '@assets': path.resolve(__dirname, './client/public/images'),
     },
   },
   build: {
-    outDir: 'dist',
+    outDir: '../dist', // Output to root-level dist folder
     emptyOutDir: true,
   },
   server: {
